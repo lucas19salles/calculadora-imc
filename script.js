b02a7aa266da9252c2f327fc84f33044070c288e
lucas19salles <lucassalles19@hotmail.com>
2023-09-11 21:11:07 -0300

    Atualizando html, css e javascript

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const btn = document.querySelector(".btn");
-
-function calculadoraImc() {
-  const altura = parseFloat(document.querySelector("#metros").value);
-  const peso = parseFloat(document.querySelector("#peso").value);
-  let resultado = document.querySelector("#resultado");
-  let imc = peso / altura ** 2;
-  resultado.value = imc.toFixed(2);
-  console.log(imc);
-  resultado.style.visibility = "visible";
-  if (isNaN(altura) || isNaN(peso)) {
-    alert("Caractere inválido, Por favor digite caractere númerico");
-  }
-}
-
-btn.addEventListener("click", (event) => {
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  /* Previne o comportamento padrão do evento submit do JS, ou seja,
+    impede o recarregamento da página */
   event.preventDefault();
-  calculadoraImc();
+
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value;
+
+  const bmi = (weight / (height * height)).toFixed(2);
+
+  if (!isNaN(bmi)) {
+    const value = document.getElementById("value");
+    let description = "";
+
+    value.classList.add("attention");
+
+    document.getElementById("infos").classList.remove("hidden");
+
+    if (bmi < 18.5) {
+      description = "Cuidado! Você está abaixo do peso!";
+    } else if (bmi >= 18.5 && bmi <= 25) {
+      description = "Você está no peso ideal!";
+      value.classList.remove("attention");
+      value.classList.add("normal");
+    } else if (bmi > 25 && bmi <= 30) {
+      description = "Cuidado! Você está com sobrepeso!";
+    } else if (bmi > 30 && bmi <= 35) {
+      description = "Cuidado! Você está com obesidade moderada!";
+    } else if (bmi > 35 && bmi <= 40) {
+      description = "Cuidado! Você está com obesidade severa!";
+    } else {
+      description = "Cuidado! Você está com obesidade morbida!";
+    }
+
+    value.textContent = bmi.replace(".", ",");
+    document.getElementById("description").textContent = description;
+  }
+});
+
+form.addEventListener("keypress", function (event) {
+  if (event.key === "press") {
+    document.getElementById("#calculate").click();
+  }
 });
